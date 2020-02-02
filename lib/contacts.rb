f7ca2cb1b9e53665986839f231277d2edf0e8d1f
Timur Mishiev <timurmishiev@gmail.com>
2020-02-02 04:02:13 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,7 +15,9 @@ contacts = {
 contacts.each do |person, contact_details_hash|
   if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
-      
+      if attribute == :favorite_ice_cream_flavors
+       
+      end
     end
   end
 end
